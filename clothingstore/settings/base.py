ea0188706403bf53a18583f7d655934ae5017dac
Big Steppa <steppacodes@gmail.com>
2023-11-05 11:53:58 -0800

    updated packages version

--- clothingstore/settings/base.py
+++ clothingstore/settings/base.py
@@ -141,13 +141,11 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-    ]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media/")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
@@ -174,11 +172,11 @@ JAZZMIN_SETTINGS = {
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_header": "CS ADMIN",
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "media/logo.jpeg",
+    "site_logo": "media/logo.jpgg",
     # CSS classes that are applied to the logo above
     "site_logo_classes": "media/logo.jpeg",
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "media/logo.jpeg",
+    "login_logo": "media/logo.jpg",
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": "media/logo.jpeg",
     # Welcome text on the login screen
