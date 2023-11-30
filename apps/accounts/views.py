ac2cfb55358d3b22925da366941a4f5c7038ce9e
Big Steppa <steppacodes@gmail.com>
2023-11-30 12:02:31 +0100

    login post and registration post

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 from django.views import View
 from .forms import RegisterForm, LoginForm
 
@@ -25,3 +27,26 @@ class LoginView(View):
         context = {"form":form}
         return render(request, 'accounts/login.html', context)
 
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email=form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=email, password=password)
+
+            if not user:
+                return redirect('login')
+            else:
+                login(request, user)
+                return redirect('home')
+            
+        context ={'form':form}
+        return render(request, 'accounts/login.html', context)
+
+# class LogoutView(View):
+#     def get(self, request):
+#         user = request.user
+#         print(user)
+#         #logout(user)
+#         return redirect('home')
